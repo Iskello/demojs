3a9bb1f324dfa1f3e05479a6b2ae2e893e194ee7
Iskello <iskello2017@gmail.com>
2023-03-28 13:40:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+
+/* const result = confirm('Are you here?');
+console.log(result); */
+
+/* const answer = prompt('How do you do?', 'Yes I do');
+console.log(answer + 5);  */
+
+
+/* const answers = [];
+answers[0] = prompt('Як ваше імя?', '');
+answers[1] = prompt('Ваша фамілія?', '');
+answers[2] = prompt('Скільки вам років?', '');
+
+console.log(typeof(answers)); */
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+
+const user = 'Ivan';
+
+alert(`Hello, ${user}, lol`); */
+
+
+let incr = 10,
+	decr = 10;
+
+/* ++incr;
+--decr; */
+
+console.log(incr++);
+console.log(decr--);
+
+
+
+
+
+
+const isChecked = true,
+	isClose = false;
+
+console.log(isChecked && isClose); 
+
+
+console.log(2 + 2 * 2 !== '6');
