0177ba997e1ddfb81cfdcddeb259b98de397050f
Iskello <iskello2017@gmail.com>
2023-03-28 21:13:35 +0300

    if

--- js/script.js
+++ js/script.js
@@ -23,26 +23,47 @@ personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
 
 
-/* Задание на урок:
+//--------------------------------------
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+if (4 == 9) {
+	console.log('Ok');
+} else {
+	console.log('Nit');
+}
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
 
-Проверить, чтобы все работало без ошибок в консоли */
 
+
+if (num < 49) {
+	console.log('Uu');
+} else if (num > 100) {
+	console.log('So much');
+} else {
+	console.log('Maybe');
+}
+
+(num === 50) ? console.log('Ok') : console.log('Error');
+
+
+let x = 10;
+let result = x > 10 ? 'більше 10' : 'менше або дорівнює 10';
+console.log(result); // виведе "більше 10"
+
+
+const num = '50';
+
+switch (num) {
+case '49':
+	console.log('Невірно');
+	break;
+case '100':
+	console.log('Невірно');
+	break;
+case '50':
+	console.log('Так');
+	break;
+default:
+	console.log('Шкода');
+	break;
+}
