8e6fd4e8d0a459c2b444755095d68f70af20c031
Iskello <iskello2017@gmail.com>
2023-03-28 23:04:44 +0300

    operator

--- js/script.js
+++ js/script.js
@@ -67,3 +67,110 @@ default:
 	console.log('Шкода');
 	break;
 }
+
+
+/* const hamburger = 5;
+const fries = null;
+
+if (hamburger && fries) {
+	console.log('Yeeeh');
+} else {
+	console.log('lol')
+}
+
+console.log((hamburger && fries)); */
+
+
+const hamburger = 1;
+const fries = 3;
+const cola = 2;
+
+if (hamburger ===3 && cola === 2 || fries === 3 && cola === 2) {
+	console.log('Замовляємо');
+} else {
+	console.log('Йдемо в інше місце');
+}
+
+console.log(hamburger >=3 && cola >=2);
+console.log(fries >= 3 && cola >= 2);
+console.log(hamburger ===3 && cola === 2 || fries === 3 && cola === 2);
+
+console.log(!0);
+
+
+
+
+
+
+
+
+console.log(hamburger || cola  || fries);
+
+if (hamburger || cola  || fries) {
+	console.log('Замовляємо');
+} else {
+	console.log('Йдемо в інше місце');
+}
+
+
+console.log(1 && 0);
+console.log(1 && 5);
+console.log(null && 0);
+console.log(0 && 'lalka');
+
+
+let johnReport, samReport, mariaReport = 'done';
+console.log(johnReport || samReport || mariaReport);
+
+
+
+console.log(NaN || 2  || undefined);
+console.log(NaN && 2  && undefined);
+console.log(1 && 2  && 3);
+console.log(!1 && 2  || !3);
+console.log(25 || null  && !3);
+
+
+console.log( NaN || null || !3 || undefined || 5);
+
+console.log( NaN || null && !3 && undefined || 5);
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
+
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+} else {
+	console.log('Lol');
+}
+
+
+
+
